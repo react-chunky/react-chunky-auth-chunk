7ed80172ba00d539b8452a41b5f632d761b55d7f
I. Dan Calinescu <idancalinescu@gmail.com>
2017-09-13 08:28:05 +0300

    fixed loading progress

--- screens/loading.js
+++ screens/loading.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Screen } from 'react-native-chunky'
 
-export default class LoginScreen extends Screen {
+export default class LoadingScreen extends Screen {
+
+    renderDataLoading() {
+      return this.renderProgress()
+    }
+
 }
